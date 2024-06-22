7aa46f2e75416ef89e5c7c969a5bf342a2206f00
Mariana Emerenciano <103092034+mariemerenc@users.noreply.github.com>
2024-06-22 18:49:29 -0300

    Create initials.js

---
+++ initials.js
@@ -0,0 +1,9 @@
+// Write code below 💖
+
+console.log("M   M EEEEE")
+console.log("MM MM E    ")
+console.log("MM MM E    ")
+console.log("M M M EEE  ")
+console.log("M   M E    ")
+console.log("M   M E    ")
+console.log("M   M EEEEE")
