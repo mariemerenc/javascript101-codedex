8efa3b9a0913247b176eb755b0c850559d8073a0
Mariana Emerenciano <103092034+mariemerenc@users.noreply.github.com>
2024-06-22 20:24:57 -0300

    Create currency.js

---
+++ currency.js
@@ -0,0 +1,11 @@
+let dollar_to_yuan = 0.14
+let dollar_to_yen = 0.0062
+let dollar_to_won = 0.00072
+
+let yuan = 715
+let yen = 645
+let won = 1389
+
+total = yuan*dollar_to_yuan + yen*dollar_to_yen + won*dollar_to_won
+
+console.log(total)
